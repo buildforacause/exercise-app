cf0c72f0dfb73b24a3b886e6241cc0b63126afb2
Harmit <bmovies52@gmail.com>
2024-04-05 02:33:37 +0530

    added recommendation and calorie count api

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,21 @@
 import os
+import pickle
+import pandas as pd
+import random
+import requests
 from flask import Flask, jsonify, request
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_bcrypt import Bcrypt
 from pymongo import MongoClient
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
 
+
+headers = {
+    "x-app-id": os.getenv("NUTRI_API_APP_ID"),
+    "x-app-key": os.getenv("NUTRI_API_KEY"),
+}
 
 app = Flask(__name__)
 
@@ -14,6 +26,16 @@ bcrypt = Bcrypt(app)
 
 client = MongoClient(os.getenv("MONGO_URI"))
 db = client.get_database("exercise_db")
+
+df = pd.read_csv("data/exercises-cleaned.csv")
+
+tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf_vectorizer.fit_transform(df['main'])
+scaler = StandardScaler()
+scaler.fit_transform(tfidf_matrix.toarray())
+
+with open("model.pkl", "rb") as file:
+    model = pickle.load(file)
 
 
 @app.route('/api/v1/users/register', methods=['POST'])
@@ -53,7 +75,52 @@ def login():
 @jwt_required()
 def logout():
     current_user = get_jwt_identity()
-    return jsonify({'message': f'Successfully logged out user {current_user}'}), 200
+    return jsonify({'success': 1, 'message': f'Successfully logged out user {current_user}'}), 200
+
+
+# todo 1 - optimise this + use mongoDB + add validations to all API's
+@app.route('/api/v1/exercises/recommend', methods=['POST'])
+def recommend():
+    data = request.get_json(silent=True)
+    if not data or 'input' not in data:
+        return jsonify({'success': 0, 'message': 'Input is required to post this route.'}), 400
+    input = [data.get("input")]
+    tfidf = tfidf_vectorizer.transform(input)
+    scaled_input = scaler.transform(tfidf.toarray())
+    _, indices = model.kneighbors(scaled_input)
+    recommended_exercises_indices = indices[0]
+    recommended_exercises_indices = recommended_exercises_indices.tolist()
+    recommended_exercises = df.iloc[recommended_exercises_indices].to_dict(orient='records')
+    exercises = random.sample(recommended_exercises, k=min(5, len(recommended_exercises)))
+    return jsonify({'success': 1, 'data': exercises, 'message': 'Data recommended successfully'}), 200
+
+
+# 1 second loading time due to external api call
+# todo 2 - optimise this
+@app.route('/api/v1/exercises/get-calories', methods=['POST'])
+def get_calories():
+    data = request.get_json(silent=True)
+    if not data:
+        return jsonify({'success': 0, 'message': 'Input is required to post this route.'}), 400
+    exercise_params = {
+        "query": data.get("query", ""),
+        "gender": data.get("gender", ""),
+        "weight_kg": float(data.get("weight", "0.00")),
+        "height_cm": float(data.get("height", "0.00")),
+        "age": int(data.get("age", "0"))
+    }
+    query = requests.post(url=os.getenv("NUTRI_API_ENDPOINT"), json=exercise_params, headers=headers)
+    data = query.json()
+    total_cal_burnt = 0
+    total_mins_spent = 0
+    exercises = []
+    for n in range(0, len(data['exercises'])):
+        exercise = [data['exercises'][n]['name'].title(), data['exercises'][n]['nf_calories'],
+                    data['exercises'][n]['duration_min']]
+        exercises.append(exercise)
+        total_cal_burnt = total_cal_burnt + data['exercises'][n]['nf_calories']
+        total_mins_spent = total_mins_spent + data['exercises'][n]['duration_min']
+        return jsonify({'success': 1, 'message': f"You've burnt {total_cal_burnt} calories and spent {total_mins_spent} minutes!"}), 200
 
 
 @app.route("/", methods=["GET"])
